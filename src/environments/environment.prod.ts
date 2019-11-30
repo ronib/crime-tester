714374d51a440042cf1faddb8b2b9324b6a0d997
Roni Bar-David <Roni.Bar-David@cellebrite.com>
2019-12-01 00:36:14 +0200

    change fav icon

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoicmVzaGJldCIsImEiOiJjazJldW83cHIwOTVvM2dvZWdrZ2Z6dHoxIn0.dC17JvoskGT6-1NlddFthw'
+  }
 };
