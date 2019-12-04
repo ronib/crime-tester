1605766349440ebb1e0fb58cd2f6e6d2654f9b7f
Roni Bar-David <Roni.Bar-David@cellebrite.com>
2019-12-04 17:16:09 +0200

    Auto-generated commit

--- src/app/core/map/map.actions.ts
+++
@@ -1,4 +0,0 @@
-import { createAction } from '@ngrx/store';
-
-export const loadMapData = createAction('[Map] Load');
-export const mapAddMarker = createAction('[Map] Add Marker');
--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  mapbox: {
-    accessToken: 'pk.eyJ1IjoicmVzaGJldCIsImEiOiJjazJldW83cHIwOTVvM2dvZWdrZ2Z6dHoxIn0.dC17JvoskGT6-1NlddFthw'
-  },
-  dataUrl: 'https://44.229.50.66:5002/data'  
-};
