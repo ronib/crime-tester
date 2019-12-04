2b3fcfc54bdb9ec83646efb701e49769b90cb034
Roni Bar-David <Roni.Bar-David@cellebrite.com>
2019-12-04 18:46:22 +0200

    current loc

--- src/app/features/map/map.component.ts
+++ src/app/features/map/map.component.ts
@@ -16,14 +16,13 @@ export class MapComponent implements OnInit {
 
   @Output() markerClicked = new EventEmitter<string>();
   inputFocus = false;
-
+  setIntervalId = null;
   map: mapboxgl.Map;
   markersData: any[];
   mock = [{ "updateTime": "2019-12-04T12:15:51.621Z", "event": "POI", "data": [{ "owner": "killer", "points": [{ "lon": "32.090280", "lat": "34.820134" }, { "lon": "32.087415", "lat": "34.812946" }, { "lon": "32.090677", "lat": "34.805180" }, { "lon": "32.091011", "lat": "34.804824" }, { "lon": "32.091155", "lat": "34.804372" }] }] }];
   constructor(private mapService: MapService,
     private renderer2: Renderer2,
     private store: Store<{ mapState: MapState }>,
-    private zone: NgZone,
 
   ) { }
 
@@ -31,11 +30,17 @@ export class MapComponent implements OnInit {
     this.setToken();
     // this.store.dispatch(loadMapData());
 
-    this.mapService.get(environment.dataUrl).subscribe((response: any) => {
-      this.markersData = response[0].data;
-      this.displayMap();
-      this.displayMarkers();
-    });
+    // this.mapService.get(environment.dataUrl).subscribe((response: any) => {
+    this.markersData = this.mock[0].data;
+    this.displayMap();
+    this.displayMarkers();
+
+
+    this.setIntervalId = setInterval(() => { this.displayCurrentLocation(); }, 2000);
+
+
+    // this.displayCurrentLocation();
+    // });
 
   }
 
@@ -44,111 +49,147 @@ export class MapComponent implements OnInit {
     Object.getOwnPropertyDescriptor(mapboxgl, "accessToken").set(environment.mapbox.accessToken);
   }
 
-  displayMap() {
-    this.map = new mapboxgl.Map(MAP_INITIAL);
+  displayCurrentLocation() {
 
-    // Add map controls
-    this.map.addControl(new mapboxgl.NavigationControl());
-  }
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const target = [pos.coords.longitude, pos.coords.latitude] as mapboxgl.LngLatLike;
+      console.log("fly to", target);
+      this.map.flyTo({
+        // These options control the ending camera position: centered at
+        // the target, at zoom level 9, and north up.
+        center: target,
+        zoom: 12,
+        bearing: 0,
 
-  addPopupToMap(popup: mapboxgl.Popup, lngLat: mapboxgl.LngLatLike, skipOpenEvent = false) {
-    return this.zone.runOutsideAngular(() => {
-      if (skipOpenEvent && (<any>popup)._listeners) {
-        delete (<any>popup)._listeners['open'];
-      }
-      popup.setLngLat(lngLat);
-      popup.addTo(this.map);
-    });
-  }
+        // These options control the flight curve, making it move
+        // slowly and zoom out almost completely before starting
+        // to pan.
+        speed: 0.2, // make the flying slow
+        curve: 1, // change the speed at which it zooms out
 
-  displayMarkers() {
-
-    const dataLine = [];
-    this.markersData.forEach(marker => {
-      marker.points.forEach(point => {
-
-        let el = this.renderer2.createElement('div');
-        el.className = 'marker';
-        const coordinate = [point.lat, point.lon] as mapboxgl.LngLatLike;
-        dataLine.push(coordinate);
-        new mapboxgl.Marker(el)
-          .setLngLat(coordinate)
-          .setPopup(new mapboxgl.Popup({ offset: 25 })
-            .setHTML('<h3>' + marker.owner + '</h3><p>' + 'description 1234567890' + '</p>')
-          )
-          .addTo(this.map);
+        // This can be any easing function: it takes a number between
+        // 0 and 1 and returns another number between 0 and 1.
+        easing: function (t) { return t; }
       });
 
+      let el = this.renderer2.createElement('div');
+      el.className = 'marker-curr';
+      new mapboxgl.Marker(el)
+        .setLngLat(target)
+        .addTo(this.map);
+
+  });
+  // const currLoc = new mapboxgl.GeolocateControl({
+  //   positionOptions: {
+  //     enableHighAccuracy: true
+  //   },
+  //   trackUserLocation: true
+  // });
+  // this.map.addControl(currLoc);
+
+  // currLoc['_geolocateButton'].click();
 
 
 
+}
+
+displayMap() {
+  this.map = new mapboxgl.Map(MAP_INITIAL);
+
+  // Add map controls
+  this.map.addControl(new mapboxgl.NavigationControl());
+}
+
+
+
+displayMarkers() {
+
+  const dataLine = [];
+  this.markersData.forEach(marker => {
+    marker.points.forEach(point => {
+
+      let el = this.renderer2.createElement('div');
+      el.className = 'marker';
+      const coordinate = [point.lat, point.lon] as mapboxgl.LngLatLike;
+      dataLine.push(coordinate);
+      new mapboxgl.Marker(el)
+        .setLngLat(coordinate)
+        .setPopup(new mapboxgl.Popup({ offset: 25 })
+          .setHTML('<h3>' + marker.owner + '</h3><p>' + 'description 1234567890' + '</p>')
+        )
+        .addTo(this.map);
     });
 
 
-    this.map.on('load', () => {
-      // Insert the layer beneath any symbol layer.
-      var layers = this.map.getStyle().layers;
-        
 
-      var labelLayerId;
-      for (var i = 0; i < layers.length; i++) {
-        if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
-          labelLayerId = layers[i].id;
-          break;
-        }
+
+  });
+
+
+  this.map.on('load', () => {
+    // Insert the layer beneath any symbol layer.
+    var layers = this.map.getStyle().layers;
+
+
+    var labelLayerId;
+    for (var i = 0; i < layers.length; i++) {
+      if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+        labelLayerId = layers[i].id;
+        break;
       }
+    }
 
-      this.map.addLayer({
-        'id': '3d-buildings',
-        'source': 'composite',
-        'source-layer': 'building',
-        'filter': ['==', 'extrude', 'true'],
-        'type': 'fill-extrusion',
-        'minzoom': 15,
-        'paint': {
-          'fill-extrusion-color': '#aaa',
+    this.map.addLayer({
+      'id': '3d-buildings',
+      'source': 'composite',
+      'source-layer': 'building',
+      'filter': ['==', 'extrude', 'true'],
+      'type': 'fill-extrusion',
+      'minzoom': 15,
+      'paint': {
+        'fill-extrusion-color': '#aaa',
 
-          // use an 'interpolate' expression to add a smooth transition effect to the
-          // buildings as the user zooms in
-          'fill-extrusion-height': [
-            "interpolate", ["linear"], ["zoom"],
-            15, 0,
-            15.05, ["get", "height"]
-          ],
-          'fill-extrusion-base': [
-            "interpolate", ["linear"], ["zoom"],
-            15, 0,
-            15.05, ["get", "min_height"]
-          ],
-          'fill-extrusion-opacity': .6
-        }
-      }, labelLayerId);
+        // use an 'interpolate' expression to add a smooth transition effect to the
+        // buildings as the user zooms in
+        'fill-extrusion-height': [
+          "interpolate", ["linear"], ["zoom"],
+          15, 0,
+          15.05, ["get", "height"]
+        ],
+        'fill-extrusion-base': [
+          "interpolate", ["linear"], ["zoom"],
+          15, 0,
+          15.05, ["get", "min_height"]
+        ],
+        'fill-extrusion-opacity': .6
+      }
+    }, labelLayerId);
 
-      this.map.addLayer({
-        "id": "route",
-        "type": "line",
-        "source": {
-          "type": "geojson",
-          "data": {
-            "type": "Feature",
-            "properties": {},
-            "geometry": {
-              "type": "LineString",
-              "coordinates": dataLine
-            }
+    this.map.addLayer({
+      "id": "route",
+      "type": "line",
+      "source": {
+        "type": "geojson",
+        "data": {
+          "type": "Feature",
+          "properties": {},
+          "geometry": {
+            "type": "LineString",
+            "coordinates": dataLine
           }
-        },
-        "layout": {
-          "line-join": "round",
-          "line-cap": "round"
-        },
-        "paint": {
-          "line-color": "blue",
-          "line-width": 7
         }
-      });
+      },
+      "layout": {
+        "line-join": "round",
+        "line-cap": "round"
+      },
+      "paint": {
+        "line-color": "blue",
+        "line-width": 7
+      }
     });
+  });
 
-  }
+}
 }
 
